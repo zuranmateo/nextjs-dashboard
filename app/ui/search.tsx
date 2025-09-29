59fbf72e6b026f09bb4abc66faf1ac61058f05c4
zuranmateo <mateo.zuran@scv.si>
2025-09-29 13:52:38 +0200

    chapter11, pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,26 @@
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
- 
-export default function Search({ placeholder }: { placeholder: string }) {
+import { useDebouncedCallback } from 'use-debounce';
 
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
+  
+  const handleSearch = useDebouncedCallback((term) => {
+  console.log(`Searching... ${term}`);
+ 
+  const params = new URLSearchParams(searchParams);
+  params.set('page', '1');
+  if (term) {
+    params.set('query', term);
+  } else {
+    params.delete('query');
   }
+  replace(`${pathname}?${params.toString()}`);
+}, 300);
+
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
