fe13769fed5010c28068b75e2eda15f757022b20
zuranmateo <mateo.zuran@scv.si>
2025-10-07 10:15:03 +0200

    chapter 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -161,6 +161,7 @@ export async function fetchInvoiceById(id: string) {
     }));
 
     return invoice[0];
+    return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoice.');
